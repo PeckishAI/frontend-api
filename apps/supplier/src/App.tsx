af0748558dec4fb924ce3d95a2982dc43c2d511e
Jules <jules.dempt@outlook.fr>
2023-08-01 10:56:31 +0200

    feat: Communication example

--- apps/supplier/src/App.tsx
+++ apps/supplier/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import { Button } from 'shared-ui';
 import viteLogo from '/vite.svg';
@@ -7,6 +7,13 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://192.168.2.1:8080/')
+      .then((res) => res.text())
+      .then((text) => setMessage(text));
+  }, []);
 
   return (
     <>
@@ -18,7 +25,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Supplier Site</h1>
+      <h1>Supplier Site -- {message}</h1>
       <div className="card">
         <Button
           onClick={() => setCount((_count) => _count + 1)}
