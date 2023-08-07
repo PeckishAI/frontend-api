e701c982b9f50d7bef7b9ce4a80420f3804f20e3
Hugo <hugo.dumartin@hotmail.fr>
2023-08-07 17:13:07 +0200

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   plugins: ['@typescript-eslint', 'react', 'prettier'],
   parserOptions: {
