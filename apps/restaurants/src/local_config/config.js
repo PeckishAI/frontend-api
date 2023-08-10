281f0f35e299a5f9531bd5e361fb6aa48971b717
Hugo <hugo.dumartin@hotmail.fr>
2023-08-10 12:38:36 +0200

    add inventory services

--- apps/restaurants/src/local_config/config.js
+++ apps/restaurants/src/local_config/config.js
@@ -1,5 +1,6 @@
 const local = {
-  API_URL: 'http://127.0.0.1:5000/api/',
+  // API_URL: 'http://127.0.0.1:5000/api/',
+  API_URL: 'https://restaurant-api-zqjpx7oxsq-ew.a.run.app/',
 };
 
 const server = {
