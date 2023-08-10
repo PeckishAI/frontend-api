82930383da73e8942ae3bd57a2d008ea3ff57a47
Hugo <hugo.dumartin@hotmail.fr>
2023-08-10 12:40:17 +0200

    add API integration

--- apps/restaurants/src/views/Inventory.tsx
+++ apps/restaurants/src/views/Inventory.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { Table, Tabs, Dropdown, Popup } from 'shared-ui';
+import { Table, Tabs, Dropdown, Popup, Input, Lottie } from 'shared-ui';
 import { ColumnDefinitionType } from 'shared-ui/components/Table/Table';
 import { DropdownOptionsDefinitionType } from 'shared-ui/components/Dropdown/Dropdown';
 import { useEffect, useState } from 'react';
@@ -10,20 +10,20 @@ type Props = {};
 
 type Ingredient = {
   id: string;
-  ingredientName: string;
+  name: string;
   theoriticalStock: number;
-  actualStock: number;
+  quantity: number;
   unit: string;
   supplier: string;
-  price: number;
+  cost: number;
   actions: void;
 };
 
 const tabs = ['Stock', 'analyses', 'Orders'];
 
 const units: DropdownOptionsDefinitionType[] = [
-  { label: 'g', value: 'g' },
   { label: 'kg', value: 'kg' },
+  { label: 'g', value: 'g' },
   { label: 'tbsp', value: 'tbsp' },
   { label: 'l', value: 'L' },
   { label: 'ml', value: 'ml' },
@@ -41,93 +41,193 @@ const Inventory = (props: Props) => {
 
   const [selectedTab, setSelectedTab] = useState(0);
   const [ingredientsList, setIngredientsList] = useState<Ingredient[]>();
-  const [selectedUnit, setSelectedUnit] = useState<{
-    [key: string]: string;
-  }>({});
-  const [selectedSupplier, setSelectedSupplier] = useState<{
-    [key: string]: string;
-  }>({});
+  const [editingRowId, setEditingRowId] = useState<string | null>();
+  const [deletingRowId, setDeletingRowId] = useState<string | null>();
+  const [editedValues, setEditedValues] = useState<Ingredient | null>(null);
   const [popupRevele, setPopupRevele] = useState(false);
+  const [loadingData, setLoadingdata] = useState(false);
 
   const toggleTab = (tabIndex: number) => {
     setSelectedTab(tabIndex);
   };
 
+  function reloadInventoryData() {
+    (async () => {
+      try {
+        setLoadingdata(true);
+        const list = await inventoryService.getIngredientList();
+        if (list.requestStatus === 200) {
+          setLoadingdata(false);
+        }
+        setIngredientsList(list.data);
+      } catch (error) {
+        console.error('Error fetching ingredient list:', error);
+      }
+    })();
+  }
   useEffect(() => {
-    inventoryService.getIngredientList().then((res) => {
-      setIngredientsList(res.data);
-    });
+    reloadInventoryData();
   }, []);
 
-  const onUnitChange = (row, event) => {
-    setSelectedUnit({
-      ...selectedUnit,
-      [row.id]: event.target.value,
-    });
+  const handleEditClick = (row: Ingredient) => {
+    setEditingRowId(row.id);
+    setEditedValues({ ...row });
   };
 
-  const onSupplierChange = (row, event) => {
-    setSelectedSupplier({
-      ...selectedSupplier,
-      [row.id]: event.target.value,
+  const handleSaveEdit = () => {
+    console.log('line edited saved'); // request to api on /update...
+    setLoadingdata(true);
+    inventoryService.updateIngredient(editedValues).then((res) => {
+      if (res.status === 200) {
+        reloadInventoryData();
+      }
     });
+
+    setEditingRowId(null);
+    setEditedValues(null);
   };
 
-  const togglePopup = () => {
+  const handleCancelEdit = () => {
+    setEditingRowId(null);
+    setEditedValues(null);
+  };
+
+  const handleDeleteClick = (row: Ingredient) => {
+    setDeletingRowId(row.id);
+    togglePopupDelete();
+  };
+
+  const handleValueChange = (field: keyof Ingredient, value: any) => {
+    setEditedValues((prevValues) => ({
+      ...prevValues!,
+      [field]: value,
+    }));
+  };
+
+  const togglePopupDelete = () => {
     setPopupRevele(!popupRevele);
   };
 
   const columns: ColumnDefinitionType<Ingredient, keyof Ingredient>[] = [
     {
-      key: 'ingredientName',
+      key: 'name',
       header: t('ingredientName'),
+      width: '15%',
       classname: 'column-bold',
+      renderItem: (row) =>
+        editingRowId === row.id ? (
+          <Input
+            type="text"
+            min={0}
+            placeholder="Quantity"
+            onChange={(value) => handleValueChange('name', value)}
+            value={editedValues!.name}
+          />
+        ) : (
+          row.name
+        ),
     },
-    { key: 'theoriticalStock', header: t('theoriticalStock') },
-    { key: 'actualStock', header: t('actualStock') },
+
+    { key: 'theoriticalStock', header: t('theoriticalStock'), width: '15%' },
+    {
+      key: 'quantity',
+      header: t('actualStock'),
+      width: '15%',
+      renderItem: (row) =>
+        editingRowId === row.id ? (
+          <Input
+            type="number"
+            min={0}
+            placeholder="Quantity"
+            onChange={(value) => handleValueChange('quantity', value)}
+            value={editedValues!.quantity}
+          />
+        ) : (
+          row.quantity
+        ),
+    },
     {
       key: 'unit',
       header: t('unit'),
-      renderItem: (row) => (
-        <Dropdown
-          options={units}
-          selectedOption={selectedUnit[row.id]}
-          onOptionChange={(e) => onUnitChange(row, e)}
-        />
-      ),
+      width: '10%',
+      renderItem: (row) =>
+        editingRowId === row.id ? (
+          <Dropdown
+            options={units}
+            selectedOption={editedValues!.unit}
+            onOptionChange={(value) => handleValueChange('unit', value)}
+          />
+        ) : (
+          row.unit
+        ),
     },
     {
       key: 'supplier',
       header: t('supplier'),
-      renderItem: (row) => (
-        <Dropdown
-          options={suppliers}
-          selectedOption={selectedSupplier[row.id]}
-          onOptionChange={(e) => onSupplierChange(row, e)}
-        />
-      ),
+      width: '15%',
+      renderItem: (row) =>
+        editingRowId === row.id ? (
+          <Dropdown
+            options={suppliers}
+            selectedOption={editedValues!.supplier}
+            onOptionChange={(value) => handleValueChange('supplier', value)}
+          />
+        ) : (
+          row.supplier
+        ),
     },
     {
-      key: 'price',
+      key: 'cost',
       header: t('price'),
-      renderItem: (row) => `${row.price} €`,
+      width: '10%',
       classname: 'column-bold',
+      renderItem: (row) =>
+        editingRowId === row.id ? (
+          <Input
+            type="number"
+            min={0}
+            placeholder={t('price')}
+            onChange={(value) => handleValueChange('cost', value)}
+            value={editedValues!.cost}
+          />
+        ) : (
+          `${row.cost} €`
+        ),
     },
     {
       key: 'actions',
       header: t('actions'),
+      width: '10%',
       renderItem: (row) => {
         return (
           <div className="actions">
-            <i
-              className="fa-solid fa-pen-to-square"
-              data-tooltip-id="actions-tooltip"
-              data-tooltip-content={t('edit')}></i>
-            <i
-              className="fa-solid fa-trash"
-              data-tooltip-id="actions-tooltip"
-              data-tooltip-content={t('delete')}
-              onClick={togglePopup}></i>
+            {editingRowId === row.id ? (
+              <i
+                className="fa-solid fa-check"
+                data-tooltip-id="actions-tooltip"
+                data-tooltip-content={t('validate')}
+                onClick={handleSaveEdit}></i>
+            ) : (
+              <i
+                className="fa-solid fa-pen-to-square"
+                data-tooltip-id="actions-tooltip"
+                data-tooltip-content={t('edit')}
+                onClick={() => handleEditClick(row)}></i>
+            )}
+
+            {editingRowId === row.id ? (
+              <i
+                className="fa-solid fa-times"
+                data-tooltip-id="actions-tooltip"
+                data-tooltip-content={t('cancel')}
+                onClick={handleCancelEdit}></i>
+            ) : (
+              <i
+                className="fa-solid fa-trash"
+                data-tooltip-id="actions-tooltip"
+                data-tooltip-content={t('delete')}
+                onClick={() => handleDeleteClick(row)}></i>
+            )}
           </div>
         );
       },
@@ -143,14 +243,25 @@ const Inventory = (props: Props) => {
       <Popup
         type="warning"
         msg="Are you sure you want to delete it ?"
-        subMsg='Cette action aura des impactes sur les éléments suivant : trousse, bonjour..."'
+        // subMsg='Cette action aura des impactes sur les éléments suivant : trousse, bonjour..."'
         onConfirm={() => {
-          console.log('supression confirmée');
-          togglePopup();
+          console.log('editing row id :', deletingRowId);
+
+          inventoryService.deleteIngredient(deletingRowId);
+          console.log('after');
+
+          togglePopupDelete();
         }}
         revele={popupRevele}
-        togglePopup={togglePopup}
+        togglePopup={togglePopupDelete}
       />
+      {loadingData && (
+        <div className="loading-middle-page-overlay">
+          <div className="loading-container">
+            <Lottie type="loading" width="200px" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
