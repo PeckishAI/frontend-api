bcd3f831f0cc0bbaa50269d2609d3d73a2319f95
Hugo <hugo.dumartin@hotmail.fr>
2023-08-11 13:04:56 +0200

    optimize services

--- apps/restaurants/src/_services/inventory.service.js
+++ apps/restaurants/src/_services/inventory.service.js
@@ -2,15 +2,8 @@ import axios from './index';
 
 let restaurantUUIDExemple = '514db708-3b64-4cb0-a048-ae1a001e68ad';
 
-let getIngredientList = async () => {
-  const res = await axios.get('/inventory/' + restaurantUUIDExemple);
-  console.log('inventory request status', res.status);
-  const convertedData = Object.keys(res.data).map((key) => ({
-    id: key,
-    theoriticalStock: 0, // Tempoprary till API implementation
-    ...res.data[key],
-  }));
-  return { data: convertedData, requestStatus: res.status };
+let getIngredientList = () => {
+  return axios.get('/inventory/' + restaurantUUIDExemple);
 };
 
 let addIngredient = (ingredient) => {
@@ -19,13 +12,18 @@ let addIngredient = (ingredient) => {
 
 let updateIngredient = (ingredient) => {
   const ingredientFormated = Object.keys(ingredient)
-    .filter((key) => key !== 'id' && key !== 'theoriticalStock')
+    .filter(
+      (key) =>
+        key !== 'id' &&
+        key !== 'theoriticalStock' &&
+        ingredient[key] !== null &&
+        ingredient[key] !== undefined &&
+        ingredient[key] !== ''
+    )
     .reduce((obj, key) => {
       obj[key] = ingredient[key];
       return obj;
     }, {});
-  console.log('without remove id :', ingredient);
-  console.log('with remove id :', ingredientFormated);
   return axios.post(
     '/inventory/' + ingredient.id + '/update',
     ingredientFormated
