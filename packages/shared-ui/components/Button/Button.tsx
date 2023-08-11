3d64af823b289ac211a39760def6685856a9b151
Hugo <hugo.dumartin@hotmail.fr>
2023-08-11 16:22:26 +0200

    add loading lottie on button

--- packages/shared-ui/components/Button/Button.tsx
+++ packages/shared-ui/components/Button/Button.tsx
@@ -1,18 +1,23 @@
 import './style.scss';
+import { Lottie } from 'shared-ui';
 
 type Props = {
   value: string;
   type: 'primary' | 'secondary';
   className?: string;
-  onClick: () => void;
+  loading?: boolean;
+  onClick?: () => void;
 };
 
 const Button = (props: Props) => {
   return (
     <button
-      className={`button ${props.type} ${props.className}`}
+      className={`button ${props.type} ${props.className ?? ''}`}
       onClick={props.onClick}>
-      {props.value}
+      <span style={props.loading ? { opacity: 0 } : undefined}>
+        {props.value}
+      </span>
+      {props.loading && <Lottie type="loading" width="45px" />}
     </button>
   );
 };
