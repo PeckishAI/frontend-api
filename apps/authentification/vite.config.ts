96ba9c094f8bce2fc321a2514ecfd4cd9b9f5e92
Jules <jules.dempt@outlook.fr>
2023-08-15 16:13:36 +0200

    feat(auth): create auth app

---
+++ apps/authentification/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 5123,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+    },
+  },
+  plugins: [react()],
+});
