3e15ccc773e9fd192892b130fba4961445a809fb
Jules <jules.dempt@outlook.fr>
2023-08-17 11:18:06 +0200

    fix(restaurant): redirect if unknown page

--- apps/restaurants/src/routes/routes.tsx
+++ apps/restaurants/src/routes/routes.tsx
@@ -1,13 +1,17 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import MyRestaurant from '../views/MyRestaurant';
-import Overview from '../views/Overview';
+import Overview from '../views/Overview/Overview';
 import Inventory from '../views/Inventory';
 import Recipes from '../views/Recipes';
 import Onboarding from '../views/Onboarding';
 import Layout from '../components/Layout';
-import { LoginHandler } from '../store/LoginHandler';
+import { LoginHandler } from './LoginHandler';
 
 export const router = createBrowserRouter([
+  {
+    index: true,
+    element: <Navigate to={'/overview'} />,
+  },
   {
     path: '/login-handler',
     element: <LoginHandler />,
