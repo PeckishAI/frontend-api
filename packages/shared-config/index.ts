f0c5b98ba010232a24f90ba78908a77ac0f4489a
Jules <jules.dempt@outlook.fr>
2023-08-22 14:03:37 +0200

    create staging confi

--- packages/shared-config/index.ts
+++ packages/shared-config/index.ts
@@ -11,6 +11,19 @@ const devConfig = {
     '167544806451-lhqrqpn83tje89en5n73af3tiu3sm11o.apps.googleusercontent.com',
 };
 
+const stagingConfig = {
+  authentificationUrl: 'https://coucou.iampeckish.com',
+  restaurantUrl: 'https://ok.iampeckish.com',
+  supplierUrl: 'https://supplier.iampeckish.com',
+
+  cookieDomain: 'iampeckish.com',
+
+  apiUrl: 'https://api-gateway-k2w3p2ptza-ew.a.run.app',
+
+  GOOGLE_CLIENT_ID:
+    '167544806451-lhqrqpn83tje89en5n73af3tiu3sm11o.apps.googleusercontent.com',
+};
+
 const prodConfig = {
   authentificationUrl: 'https://platform.iampeckish.com',
   restaurantUrl: 'https://restaurant.iampeckish.com',
@@ -25,4 +38,8 @@ const prodConfig = {
 };
 
 export const GLOBAL_CONFIG =
-  process.env.NODE_ENV === 'development' ? devConfig : prodConfig;
+  import.meta.env.MODE === 'development'
+    ? devConfig
+    : import.meta.env.VITE_CONFIG_MODE === 'prod'
+    ? prodConfig
+    : stagingConfig;
