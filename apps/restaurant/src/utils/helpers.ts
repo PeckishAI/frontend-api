46921dbb2838f2987ad98f05904823bbfa87d055
Jules <jules.dempt@outlook.fr>
2023-08-22 14:03:25 +0200

    fix favicon

--- apps/restaurant/src/utils/helpers.ts
+++ apps/restaurant/src/utils/helpers.ts
@@ -2,7 +2,9 @@ export const prettyNumber = (x: number | string | undefined | null) => {
   if (x === undefined || x === null) return '--';
 
   if (typeof x === 'string') x = parseFloat(x);
-  x = x.toFixed(2);
+
+  // Round to 2 decimals if neccessary
+  x = Math.round((x + Number.EPSILON) * 100) / 100;
 
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 };
