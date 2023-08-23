d1763a9938d931139e8e176b813b5c9d2ec9beab
Hugo <hugo.dumartin@hotmail.fr>
2023-08-23 20:17:36 +0200

    add integrations component into onboarding view

--- apps/restaurant/src/views/Onboarding/Onboarding.tsx
+++ apps/restaurant/src/views/Onboarding/Onboarding.tsx
@@ -4,15 +4,9 @@ import { Navigate } from 'react-router-dom';
 import './Onboarding.scss';
 import Integrations from '../Integrations/Integrations';
 
-type Integration = {
-  name: string;
-  restaurantNumber: number;
-};
-
 const Onboarding = () => {
   const { t } = useTranslation('common');
   const { user } = useUserStore();
-  const [integrated, setIntegrated] = useState<Integration[]>([]);
 
   if (user && user.onboarded) {
     return <Navigate to="/" />;
