16bb789e06dc5999b66da58aa3a24c69bb69e7fd
Hugo <hugo.dumartin@hotmail.fr>
2023-08-28 18:17:36 +0200

    sidebar responsive

--- packages/shared-ui/components/Sidebar/Sidebar.tsx
+++ packages/shared-ui/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,27 @@
+import Lottie from 'react-lottie';
 import './sidebar.scss';
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import bruger from '../../asset/lotties/burger.json';
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Sidebar = (props: Props) => {
+  const [burgerClicked, setBurgerClicked] = useState(false);
+
   return (
-    <div className="sidebar">
+    <div className={`sidebar ${burgerClicked ? 'sidebar-floating' : ''}`}>
+      {burgerClicked ? (
+        <i
+          className="fa-solid fa-xmark burger"
+          onClick={() => setBurgerClicked(!burgerClicked)}></i>
+      ) : (
+        <i
+          className="fa-solid fa-bars burger"
+          onClick={() => setBurgerClicked(!burgerClicked)}></i>
+      )}
+
       <h1 className="company-name">peckish</h1>
       <div className="sidebar-items-container">{props.children}</div>
     </div>
