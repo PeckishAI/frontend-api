0cf969e6a17ed93da03853acce54049308b0467d
Hugo <hugo.dumartin@hotmail.fr>
2023-08-30 14:59:24 +0200

    start translation

--- apps/restaurant/src/translation/fr/index.ts
+++ apps/restaurant/src/translation/fr/index.ts
@@ -1,9 +1,11 @@
 import common from './common.json';
 import overview from './overview.json';
+import ingredient from './ingredient.json';
 import language from '../language';
 
 export default {
   common,
+  ingredient,
   language,
   overview,
 };
