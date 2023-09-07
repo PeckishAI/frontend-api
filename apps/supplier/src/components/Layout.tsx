b8bea27e142097878a53ea9ee60a52167ba7d06c
Hugo <hugo.dumartin@hotmail.fr>
2023-09-07 12:19:10 +0200

    added title and logout to layout

--- apps/supplier/src/components/Layout.tsx
+++ apps/supplier/src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
-import { useState } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Navigate, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import {
   Sidebar,
   SidebarItem,
@@ -8,16 +8,66 @@ import {
   Navbar,
   Lottie,
 } from 'shared-ui';
+import { useUserStore } from 'user-management';
 
 // type Props = {};
 
 const Layout = () => {
-  const { t } = useTranslation('common');
-  const navigate = useNavigate();
+  const { t } = useTranslation();
   const [isRefreshing, setIsRefreshing] = useState(false);
+  const [navTitle, setNavTitle] = useState('');
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const { logout, user } = useUserStore();
+  // const {
+  //   selectedRestaurantUUID,
+  //   setSelectedRestaurantUUID,
+  //   restaurants,
+  //   loadRestaurants,
+  //   restaurantsLoading,
+  // } = useRestaurantStore();
+
+  // useEffect(() => {
+  //   loadRestaurants();
+  // }, [loadRestaurants]);
+
   const handleIconClick = () => {
-    setIsRefreshing(true);
+    // if (!selectedRestaurantUUID) return;
   };
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  useEffect(() => {
+    // Convert path to dot notation (ex :user.profile)
+    let path = pathname;
+    if (pathname.slice(-1) === '/') {
+      path = pathname.slice(0, -1);
+    }
+    if (pathname.charAt(0) === '/') {
+      path = path.substring(1);
+    }
+    path = path.replaceAll('/', '.');
+
+    if (path === '') path = 'myRestaurant';
+
+    // Get possible translation
+    const title: string = t(path as unknown as TemplateStringsArray, {
+      defaultValue: '',
+    });
+    setNavTitle(title !== '' ? title : path);
+  }, [t, pathname]);
+
+  // if (user && !user.onboarded) {
+  //   return <Navigate to="/onboarding" />;
+  // }
+
+  // const restaurantsOptions = restaurants.map((restaurant) => ({
+  //   label: restaurant.name,
+  //   value: restaurant.uuid,
+  // }));
   return (
     <>
       <Sidebar>
@@ -60,6 +110,8 @@ const Layout = () => {
       </Sidebar>
       <div className="main">
         <Navbar
+          title={navTitle}
+          onLogout={handleLogout}
           refreshIcon={
             isRefreshing ? (
               <Lottie width="30px" type="validate" />
