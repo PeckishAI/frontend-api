f1f704d68c40b53f9cc2f12c3617a05037d58185
Hugo <hugo.dumartin@hotmail.fr>
2023-09-29 13:33:59 +0200

    map service

--- apps/supplier/src/services/map.service.ts
+++ apps/supplier/src/services/map.service.ts
@@ -1,5 +1,11 @@
 import axios from './index';
 
+export type ResponseMapPlaceApi = {
+  name: string;
+  address: string;
+  location: { lat: number; lng: number };
+};
+
 const getAutocompletePlaces = (location: string) => {
   return axios.get('http://127.0.0.1:8080/api/place/autocomplete/' + location);
 };
