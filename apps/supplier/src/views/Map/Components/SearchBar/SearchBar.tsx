f26790f35353a1d6b032d83af05c3051322cf5a7
Hugo <hugo.dumartin@hotmail.fr>
2023-09-29 13:33:09 +0200

    map search bar component

---
+++ apps/supplier/src/views/Map/Components/SearchBar/SearchBar.tsx
@@ -0,0 +1,58 @@
+import { Input } from 'shared-ui';
+import './style.scss';
+import { useState } from 'react';
+import { mapService, ResponseMapPlaceApi } from '../../../../services';
+
+type Props = {
+  onSuggestedPlaceClick: (place: ResponseMapPlaceApi) => void;
+};
+
+const SearchBar = (props: Props) => {
+  const [researchPlace, setResearchPlace] = useState<string>('');
+  const [autocompletePlaces, setAutocompletePLaces] = useState<
+    ResponseMapPlaceApi[]
+  >([]);
+
+  const handleResearchPlaceChange = (value: string) => {
+    setResearchPlace(value);
+    mapService
+      .getAutocompletePlaces(value)
+      .then((res) => {
+        let places: ResponseMapPlaceApi[] = [];
+        res.data.results.forEach((element) => {
+          places.push({
+            name: element.name,
+            address: element.formatted_address,
+            location: element.geometry.location,
+          });
+        });
+        setAutocompletePLaces(places);
+      })
+      .catch((err) => {});
+  };
+
+  return (
+    <div className="search-bar">
+      <i className="fa-solid fa-location-dot location-icon"></i>
+      <Input
+        placeholder="Enter restaurant name or city place"
+        value={researchPlace}
+        onChange={handleResearchPlaceChange}
+        className="map-input"
+      />
+      <div className="suggestion-dropdown">
+        <ul>
+          {autocompletePlaces.map((place, i) => (
+            <li key={i} onClick={() => props.onSuggestedPlaceClick(place)}>
+              <p className="name">{place.name}</p>
+              <p className="location">{place.address}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <i className="fa-solid fa-magnifying-glass search-icon"></i>
+    </div>
+  );
+};
+
+export default SearchBar;
