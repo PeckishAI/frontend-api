1acfc7932ab14a4df4e2bd32c8f8775ad7fd5196
Hugo <hugo.dumartin@hotmail.fr>
2023-09-29 13:33:45 +0200

    add features to map

--- apps/supplier/src/views/Map/Map.tsx
+++ apps/supplier/src/views/Map/Map.tsx
@@ -1,11 +1,14 @@
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
-import { Map as LeafletMap, divIcon } from 'leaflet';
+import { LatLngExpression, Map as LeafletMap } from 'leaflet';
+import hexGrid from '@turf/hex-grid';
+import { BBox2d } from '@turf/helpers/dist/js/lib/geojson';
 import 'leaflet/dist/leaflet.css'; // Importez les styles Leaflet
 import './style.scss';
 import POIWindow from './Components/POIWindow/POIWindow';
-import { useRef, useState } from 'react';
-import { Input } from 'shared-ui';
-import { mapService } from '../../services';
+import Hexagon from './Components/Hexagon/Hexagon';
+import { useEffect, useRef, useState } from 'react';
+import { ResponseMapPlaceApi } from '../../services';
+import SearchBar from './Components/SearchBar/SearchBar';
 
 type RestaurantMap = {
   name: string;
@@ -13,11 +16,6 @@ type RestaurantMap = {
   pos: { lat: number; lng: number };
 };
 
-type ResponseMapPlaceApi = {
-  name: string;
-  address: string;
-  location: { lat: number; lng: number };
-};
 const restaurantLocations: RestaurantMap[] = [
   {
     name: 'Restaurant A',
@@ -58,25 +56,56 @@ function CustomZoomButtons() {
     </div>
   );
 }
-const markerIcon = divIcon({
-  html: '<i class="fa fa-map-marker fa-4x">',
-  iconSize: [40, 40],
-  className: 'myDivIcon',
-  iconAnchor: [0, 0],
-});
 
 const Map = () => {
   const [clickedRestaurant, setClickRestaurant] = useState<
     RestaurantMap | undefined
   >(undefined);
-  const [placeResearch, setplaceResearch] = useState<string>('');
-  const [autocompletePlaces, setAutocompletePLaces] = useState<
-    ResponseMapPlaceApi[]
-  >([]);
   const [clickedPlace, setClickedPlace] = useState<
     ResponseMapPlaceApi | undefined
   >(undefined);
   const mapRef = useRef<LeafletMap | null>(null);
+  const [mapIsReady, setMapIsReady] = useState(false);
+  const [hexagons, setHexagons] = useState<LatLngExpression[][]>([]);
+  const [clickedHexagon, setClickedHexagon] = useState<
+    LatLngExpression[] | undefined
+  >(undefined);
+
+  useEffect(() => {
+    if (!mapRef.current || !mapIsReady) return;
+    const map = mapRef.current;
+
+    const getMapCornersCoordinates = () => {
+      const bounds = mapRef.current?.getBounds();
+
+      const bbox: BBox2d = [
+        bounds!.getSouthWest().lng,
+        bounds!.getSouthWest().lat,
+        bounds!.getNorthEast().lng,
+        bounds!.getNorthEast().lat,
+      ]; // [minX, minY, maxX, maxY]
+
+      const hexagon = hexGrid(bbox, 1, { units: 'kilometers' });
+      console.log('hexgon: ', hexagon);
+
+      const deuxiemeehexagonmgl: LatLngExpression[][] = hexagon.features.map(
+        (feature) =>
+          feature.geometry.coordinates[0].map(([lon, lat]) => [lat, lon])
+      );
+
+      setHexagons(deuxiemeehexagonmgl);
+    };
+
+    map.on('load', () => {
+      getMapCornersCoordinates();
+    });
+
+    map.on('moveend', () => {
+      getMapCornersCoordinates();
+    });
+
+    getMapCornersCoordinates();
+  }, [mapIsReady]);
 
   const handleMarkerClicked = (index: number) => {
     setClickRestaurant(restaurantLocations[index]);
@@ -86,29 +115,23 @@ const Map = () => {
     setClickRestaurant(undefined);
   };
 
-  const handlePlaceReasearchChange = (value: string) => {
-    setplaceResearch(value);
-    mapService
-      .getAutocompletePlaces(value)
-      .then((res) => {
-        let places: ResponseMapPlaceApi[] = [];
-        res.data.results.forEach((element) => {
-          places.push({
-            name: element.name,
-            address: element.formatted_address,
-            location: element.geometry.location,
-          });
-        });
-        setAutocompletePLaces(places);
-      })
-      .catch((err) => {});
+  const handleClickedPlaceChange = (place: ResponseMapPlaceApi) => {
+    setClickedPlace(place);
+    mapRef.current?.setView([place.location.lat, place.location.lng], 14);
+  };
+
+  const handleOnHexagonClick = () => {
+    setClickRestaurant(restaurantLocations[0]);
   };
 
   return (
     <div className="map">
       <MapContainer
         className="map-component"
-        ref={mapRef}
+        ref={(ref) => {
+          mapRef.current = ref;
+          setMapIsReady(true);
+        }}
         center={
           clickedPlace !== undefined
             ? [clickedPlace.location.lat, clickedPlace.location.lng]
@@ -120,7 +143,6 @@ const Map = () => {
         zoomControl={false}
         doubleClickZoom={true}>
         <TileLayer
-          // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://api.mapbox.com/styles/v1/peckishmap/clmoz2ujr01zr01qufplwc2pl/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGVja2lzaG1hcCIsImEiOiJjbG1veXhheXYwMTJrMnNuMnY1bDlldWQxIn0.U8tKZ1H0NJyM0BQCz9zS8w"
           maxZoom={19}
         />
@@ -137,39 +159,17 @@ const Map = () => {
             </Popup>
           </Marker>
         ))}
-
         <CustomZoomButtons />
+        {hexagons.map((hexagon, i) => (
+          <Hexagon
+            key={i}
+            hexagon={hexagon}
+            onHexagonClick={handleOnHexagonClick}
+          />
+        ))}
       </MapContainer>
       <POIWindow onClose={handleOnWindowClose} restaurant={clickedRestaurant} />
-      <div className="toolbar">
-        <i className="fa-solid fa-location-dot location-icon"></i>
-        <Input
-          placeholder="Enter restaurant name or city place"
-          value={placeResearch}
-          onChange={handlePlaceReasearchChange}
-          className="map-input"
-        />
-        <div className="suggestion-dropdown">
-          <ul>
-            {autocompletePlaces.map((place, i) => (
-              <li
-                key={i}
-                onClick={() => {
-                  setClickedPlace(place);
-                  setplaceResearch(place.name);
-                  mapRef.current?.setView(
-                    [place.location.lat, place.location.lng],
-                    14
-                  );
-                }}>
-                <p className="name">{place.name}</p>
-                <p className="location">{place.address}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <i className="fa-solid fa-magnifying-glass search-icon"></i>
-      </div>
+      <SearchBar onSuggestedPlaceClick={handleClickedPlaceChange} />
     </div>
   );
 };
