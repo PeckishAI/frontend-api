c2bae79ac6bc4f27565a2d957cb7c8ef9de31933
Hugo <hugo.dumartin@hotmail.fr>
2023-10-04 15:02:18 +0200

    add replace burger menu with created component

--- apps/supplier/src/views/Map/Components/ControlLayer/ControlLayer.tsx
+++ apps/supplier/src/views/Map/Components/ControlLayer/ControlLayer.tsx
@@ -1,5 +1,7 @@
-import { Switch } from 'shared-ui';
+import { BurgerMenu, Switch } from 'shared-ui';
 import './style.scss';
+import { useState } from 'react';
+import classNames from 'classnames';
 
 type Props = {
   hexagonEnable: boolean;
@@ -7,21 +9,18 @@ type Props = {
 };
 
 function ControlLayer(props: Props) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="control-layer">
-      <input type="checkbox" id="menu" />
-      <label htmlFor="menu" className="burger">
-        <div className="bar leftBar"></div>
-        <div className="bar"></div>
-        <div className="bar rightBar"></div>
-      </label>
-      <div className="menu">
+      <BurgerMenu isOpen={isOpen} toggle={() => setIsOpen(!isOpen)} />
+      <div className={classNames('menu', isOpen && 'open')}>
         <div className="options">
           <div className="option">
             <span>Hexagons</span>
             <Switch
               isActive={props.hexagonEnable}
               toggle={props.onToogleHexagon}
+              width={36}
             />
           </div>
         </div>
