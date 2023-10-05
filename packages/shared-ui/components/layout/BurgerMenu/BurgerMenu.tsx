1e34295417c974848d18efba50b6c803107eb63b
Hugo <hugo.dumartin@hotmail.fr>
2023-10-05 11:28:26 +0200

    fix burger menu

--- packages/shared-ui/components/layout/BurgerMenu/BurgerMenu.tsx
+++ packages/shared-ui/components/layout/BurgerMenu/BurgerMenu.tsx
@@ -1,19 +1,22 @@
-import './style.scss'
+import './style.scss';
 import classNames from 'classnames';
 
 type Props = {
-    isOpen: boolean;
-    toggle:()=>void;
-}
+  isOpen: boolean;
+  toggle: () => void;
+  className?: string;
+};
 
 const BurgerMenu = (props: Props) => {
   return (
-    <div className={classNames('burger', props.isOpen && 'open')} onClick={props.toggle}>
-        <div className="bar leftBar"></div>
-        <div className="bar"></div>
-        <div className="bar rightBar"></div>
+    <div
+      className={classNames('burger', props.isOpen && 'open', props.className)}
+      onClick={props.toggle}>
+      <div className="bar leftBar"></div>
+      <div className="bar"></div>
+      <div className="bar rightBar"></div>
     </div>
-  )
-}
+  );
+};
 
-export default BurgerMenu
+export default BurgerMenu;
