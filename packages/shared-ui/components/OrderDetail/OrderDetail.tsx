e463a776d672bbf5a1b8399c09b08a7351177dbf
Hugo <hugo.dumartin@hotmail.fr>
2023-11-02 16:16:27 +0100

    make OrderDetail generic

--- packages/shared-ui/components/OrderDetail/OrderDetail.tsx
+++ packages/shared-ui/components/OrderDetail/OrderDetail.tsx
@@ -11,20 +11,6 @@ type Ingredient = {
   price: number;
 };
 
-// Generate 5 ingredients
-const ingredientList: Ingredient[] = [
-  { name: "Tomatoes", quantity: 3, unit: "kg", price: 2.99 },
-  { name: "Chicken", quantity: 600, unit: "g", price: 5.49 },
-  { name: "Onions", quantity: 30, unit: "unit", price: 1.29 },
-  { name: "Bell Peppers", quantity: 250, unit: "g", price: 2.99 },
-  { name: "Rice", quantity: 8, unit: "kg", price: 1.99 },
-  { name: "Carrots", quantity: 400, unit: "g", price: 1.49 },
-  { name: "Potatoes", quantity: 700, unit: "g", price: 2.79 },
-  { name: "Green Beans", quantity: 300, unit: "g", price: 2.49 },
-  { name: "Canned Tuna", quantity: 150, unit: "g", price: 2.29 },
-  { name: "Lentils", quantity: 250, unit: "g", price: 1.79 },
-]
-
 type Props<T> = {
   isVisible: boolean;
   onRequestClose: () => void;
@@ -34,15 +20,13 @@ type Props<T> = {
     value: string | number;
   }[];
   tableHeaders: ColumnDefinitionType<T>[];
-  tableData: T[]
+  tableData: T[];
 };
-
 
 export const OrderDetail = <T extends object>(props: Props<T>) => {
   useEffect(() => {
     console.log('Retrieve order :', props.orderUUID);
   }, [props.orderUUID]);
-  console.log(ingredientList);
 
   return (
     <SidePanel
@@ -51,11 +35,11 @@ export const OrderDetail = <T extends object>(props: Props<T>) => {
       onRequestClose={props.onRequestClose}
       className={styles.sidePanel}>
       <div className={styles.infosContainer}>
-        {props.upperBanner.map((info) =>(
+        {props.upperBanner.map((info) => (
           <div className={styles.info}>
-           <p className={styles.infoTitle}>{info.title}</p>
-           <p className={styles.infoValue}>{info.value}</p>
-         </div>
+            <p className={styles.infoTitle}>{info.title}</p>
+            <p className={styles.infoValue}>{info.value}</p>
+          </div>
         ))}
       </div>
       <Table columns={props.tableHeaders} data={props.tableData} />
