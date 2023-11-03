04466ea280d31d1c12ca32e8d0194bd2c4a32323
Hugo <hugo.dumartin@hotmail.fr>
2023-11-03 11:45:58 +0100

    recipeCard: fix metrics style

--- apps/restaurant/src/views/Recipes/Components/RecipeCard/RecipeCard.tsx
+++ apps/restaurant/src/views/Recipes/Components/RecipeCard/RecipeCard.tsx
@@ -2,6 +2,7 @@ import './style.scss';
 import { useTranslation } from 'react-i18next';
 import { Recipe } from '../../../../services';
 import { PiWarningCircleBold } from 'react-icons/pi';
+import { formatCurrency } from '../../../../utils/helpers';
 
 type Props = {
   recipe: Recipe;
@@ -33,9 +34,7 @@ const RecipeCard = (props: Props) => {
             className="fa-solid fa-tag price"
             data-tooltip-content={t('price')}
             data-tooltip-id="recipeCard-tooltip"></i>
-          <p>
-            {props.recipe.price} {currency}
-          </p>
+          <p>{formatCurrency(props.recipe.price, currency)}</p>
         </div>
 
         <div className="metric">
@@ -43,9 +42,7 @@ const RecipeCard = (props: Props) => {
             className="fa-solid fa-hand-holding-dollar cost"
             data-tooltip-content={t('cost')}
             data-tooltip-id="recipeCard-tooltip"></i>
-          <p>
-            {props.recipe.cost} {currency}
-          </p>
+          <p>{formatCurrency(props.recipe.cost, currency)}</p>
         </div>
 
         <div className="metric">
@@ -53,9 +50,7 @@ const RecipeCard = (props: Props) => {
             className="fa-solid fa-arrow-up-right-dots margin"
             data-tooltip-content={t('margin')}
             data-tooltip-id="recipeCard-tooltip"></i>
-          <p>
-            {props.recipe.margin} {currency}
-          </p>
+          <p>{formatCurrency(props.recipe.margin, currency)}</p>
         </div>
       </div>
     </div>
