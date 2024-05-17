8cad4378917a49346f95a6afda8c1ad931ea76c6
MayurK <mayurk.mmt@gmail.com>
2024-05-17 23:36:41 +0530

    fix(small-push): push 6

--- apps/restaurant/src/views/Overview/Overview.tsx
+++ apps/restaurant/src/views/Overview/Overview.tsx
@@ -1,23 +1,25 @@
 import { FaRegMoneyBillAlt } from 'react-icons/fa';
-import { HiOutlineUserGroup } from 'react-icons/hi';
 import { TrendCard, TrendCardSkeleton } from './components/TrendCard';
 import { formatCurrency, prettyNumber } from '../../utils/helpers';
 import styles from './Overview.module.scss';
-import { ForecastCard } from './components/ForecastCard';
+import { CostOfSalesCard } from './components/CostOfSalesCard';
 import { Tooltip } from 'react-tooltip';
 import { useEffect, useState } from 'react';
 import overviewService, {
-  Forecast,
+  ApiResponse,
+  CostofSales,
   MetricType,
-  RestaurantMetric,
 } from '../../services/overview.service';
 import { useRestaurantStore } from '../../store/useRestaurantStore';
 import { useTranslation } from 'react-i18next';
 import { EmptyPage, useTitle } from 'shared-ui';
 import { TFunction } from 'i18next';
+import DateRangePickerComponent from '../../components/DateRangePicker/DateRangePicker';
+import { format } from 'date-fns';
+import Vector from '../../assets/img/categories/Vector.svg';
 
-const metricIcon: { [K in keyof RestaurantMetric]: React.ReactNode } = {
-  occupancy: <HiOutlineUserGroup />,
+const metricIcon: { [K in keyof ApiResponse]: React.ReactNode } = {
+  costofgoodsold: <img src={Vector} />,
   sales: <FaRegMoneyBillAlt />,
 };
 
@@ -31,7 +33,7 @@ export const metricFormat: {
     currency?: string | null;
   }) => string;
 } = {
-  occupancy: ({ value, t }) =>
+  costofgoodssold: ({ value, t }) =>
     t('trend.people', {
       count: value ?? 0,
       formattedCount: prettyNumber(value),
@@ -46,10 +48,10 @@ const Overview = () => {
   useTitle(t('common:pages.overview'));
 
   const [loadingMetrics, setLoadingMetrics] = useState(false);
-  const [metrics, setMetrics] = useState<RestaurantMetric>();
-
-  const [loadingForecast, setLoadingForecast] = useState(false);
-  const [forecast, setForecast] = useState<Forecast>();
+  const [cost, setcost] = useState<ApiResponse>();
+  const [loadingCostOfSales, setLoadingCostOfSales] = useState(false);
+  const [costOfSales, setCostOfSales] = useState<CostofSales>();
+  const [value, setValue] = useState([null, null]);
 
   const { selectedRestaurantUUID, restaurants } = useRestaurantStore();
 
@@ -59,27 +61,39 @@ const Overview = () => {
 
   useEffect(() => {
     if (!selectedRestaurantUUID) return;
+    if (value[0] && value[1]) {
+      setLoadingMetrics(true);
+      overviewService
+        .getCostMetric(
+          selectedRestaurantUUID,
+          format(value[0], 'yyyy-MM-dd'),
+          format(value[1], 'yyyy-MM-dd')
+        )
+        .then((res) => {
+          if (res) {
+            setcost(res);
+          }
+        })
+        .finally(() => {
+          setLoadingMetrics(false);
+        });
 
-    setLoadingMetrics(true);
-    overviewService
-      .getMetrics(selectedRestaurantUUID)
-      .then((res) => {
-        setMetrics(res);
-      })
-      .finally(() => {
-        setLoadingMetrics(false);
-      });
-
-    setLoadingForecast(true);
-    overviewService
-      .getForecast(selectedRestaurantUUID)
-      .then((res) => {
-        setForecast(res);
-      })
-      .finally(() => {
-        setLoadingForecast(false);
-      });
-  }, [selectedRestaurantUUID]);
+      overviewService
+        .getCostOfSales(
+          selectedRestaurantUUID,
+          format(value[0], 'yyyy-MM-dd'),
+          format(value[1], 'yyyy-MM-dd')
+        )
+        .then((res) => {
+          if (res) {
+            setCostOfSales(res);
+          }
+        })
+        .finally(() => {
+          setLoadingCostOfSales(false);
+        });
+    }
+  }, [selectedRestaurantUUID, value[0], value[1]]);
 
   return (
     <>
@@ -88,27 +102,37 @@ const Overview = () => {
           <div className={styles.trends}>
             {loadingMetrics && [1, 2].map((i) => <TrendCardSkeleton key={i} />)}
             {!loadingMetrics &&
-              metrics &&
-              (Object.keys(metrics) as (keyof RestaurantMetric)[]).map(
-                (key) => (
-                  <TrendCard
-                    key={key}
-                    title={t(key)}
-                    value={metricFormat[key]({
-                      value: metrics[key].value,
-                      t,
-                      currency: currentCurrency,
-                    })}
-                    icon={metricIcon[key]}
-                    percentage={metrics[key].mom}
-                  />
-                )
-              )}
+              cost &&
+              Object.keys(cost).map((key) => (
+                <TrendCard
+                  key={key}
+                  title={key === 'costofgoodsold' ? 'Cost of Goods Sold' : key}
+                  value={cost[key]?.value?.toFixed(2) || 0}
+                  icon={metricIcon[key]}
+                  percentage={cost[key]?.percentage}
+                />
+              ))}
+            {!loadingMetrics &&
+              !cost &&
+              [1, 2].map((key) => (
+                <TrendCard
+                  key={key}
+                  title={key === 1 ? 'Cost of Goods Sold' : 'Sales'}
+                  value="0"
+                  icon={metricIcon[key === 1 ? 'costofgoodsold' : 'sales']}
+                  percentage="0"
+                />
+              ))}
+            <div className={styles.datepicker}>
+              <DateRangePickerComponent setValue={setValue} value={value} />
+            </div>
           </div>
-          <ForecastCard
-            data={forecast}
-            loading={loadingForecast}
+          <CostOfSalesCard
+            data={costOfSales}
+            loading={loadingCostOfSales}
+            selectedRestaurantUUID={selectedRestaurantUUID}
             currency={currentCurrency}
+            value={value}
           />
         </>
       ) : (
@@ -117,6 +141,7 @@ const Overview = () => {
           description={t('common:myrestaurants.emptyText')}
         />
       )}
+
       <Tooltip id="overview-tooltip" />
     </>
   );
