ee07bb6fbf5cd8a7e673b49d47236b92658f5413
mayurk14 <mayurkarmurupleta14@gmail.com>
2024-08-16 11:02:34 +0530

    fix: redCat Api Integration

--- apps/restaurant/src/views/Integrations/Components/LoginModal/LoginModal.tsx
+++ apps/restaurant/src/views/Integrations/Components/LoginModal/LoginModal.tsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from 'react';
 import './style.scss';
 import { Button, LabeledInput, Lottie, Popup } from 'shared-ui';
 import { useTranslation } from 'react-i18next';
-import { axiosClient } from '../../../../services';
+import { axiosClient, axiosIntegrationClient } from '../../../../services';
 import { useUserStore } from '@peckishai/user-management';
 import { POS, Integration } from '../../Integrations';
 import { useOAuth2 } from '../../../../utils/oauth/useOAuth2';
 import { useRestaurantStore } from '../../../../store/useRestaurantStore';
+import RedCatModal from '../RedCat/RedCatModal';
 
 type Props = {
   isVisible: boolean;
@@ -19,6 +20,9 @@ const LoginModal = (props: Props) => {
   const { t } = useTranslation(['common', 'validation', 'onboarding']);
   const userId = useUserStore((state) => state.user?.user.user_uuid);
 
+  const [redCatModalVisible, setRedCatModalVisible] = useState(false);
+  const [redCatData, setRedCatData] = useState([]);
+
   const [userName, setUserName] = useState<string>('');
   const [userPassword, setUserPassword] = useState<string>('');
   const [errorField, setErrorField] = useState('');
@@ -28,6 +32,11 @@ const LoginModal = (props: Props) => {
   const [integrated, setIntegrated] = useState<Integration>();
 
   const { selectedRestaurantUUID } = useRestaurantStore();
+
+  const redCatModal = () => {
+    setRedCatModalVisible(!redCatModalVisible);
+    setRetrieveDataStatus(null);
+  };
 
   const { getAuth, error, loading, data } = useOAuth2({
     authorizeUrl:
@@ -87,27 +96,41 @@ const LoginModal = (props: Props) => {
     }
 
     if (FieldsValid() && userId) {
-      const integrationUrl =
-        props.pos?.name === 'red_cat'
-          ? `${props?.pos?.url}/${userId}`
-          : `${props?.pos?.url}/integrate/${userId}`;
+      let integrationUrl;
+      let client;
+      if (props.pos?.name === 'red_cat') {
+        integrationUrl = `/pos/red-cat/list-store/${userId}`;
+        client = axiosIntegrationClient;
+      } else if (props.pos?.name === 'vitamojo') {
+        integrationUrl = `/${selectedRestaurantUUID}/user/${userId}`;
+        client = axiosIntegrationClient;
+      } else {
+        integrationUrl = `${props?.pos?.url}/integrate/${userId}`;
+        client = axiosClient;
+      }
 
       setRetrieveDataStatus('loading');
 
-      axiosClient
+      client
         .post(integrationUrl, {
           username: userName,
           password: userPassword,
         })
         .then((res) => {
-          setIntegrated({
-            name: res.data[0],
-            restaurantNumber: res.data.length,
-          });
-          setRetrieveDataStatus('success');
-          setTimeout(() => {
-            window.location.reload();
-          }, 2000);
+          console.log('integrationUrl', res);
+          if (props.pos?.name === 'red_cat') {
+            setRedCatModalVisible(true);
+            setRedCatData(res);
+          } else {
+            setIntegrated({
+              name: res.data[0],
+              restaurantNumber: res.data.length,
+            });
+            setRetrieveDataStatus('success');
+            setTimeout(() => {
+              window.location.reload();
+            }, 2000);
+          }
         })
         .catch((err) => {
           console.log(err);
@@ -118,108 +141,118 @@ const LoginModal = (props: Props) => {
   };
 
   return (
-    <Popup
-      isVisible={props.isVisible}
-      onRequestClose={props.toggleModal}
-      title={t('onboarding:onboarding.modal.title')}
-      subtitle={t(
-        props.pos?.auth_type === 'modal'
-          ? 'onboarding:onboarding.modal.description.login'
-          : 'onboarding:onboarding.modal.description.oauth',
-        { name: props.pos?.display_name }
-      )}>
-      {retrieveDataStatus === null ? (
-        <div className="modal-content">
-          {props.pos?.auth_type !== 'oauth' ? (
-            <>
-              <LabeledInput
-                type="text"
-                value={userName}
-                placeholder="Username"
-                onChange={(e) => setUserName(e.target.value)}
-                lighter
+    <>
+      <Popup
+        isVisible={props.isVisible}
+        onRequestClose={props.toggleModal}
+        title={t('onboarding:onboarding.modal.title')}
+        subtitle={t(
+          props.pos?.auth_type === 'modal'
+            ? 'onboarding:onboarding.modal.description.login'
+            : 'onboarding:onboarding.modal.description.oauth',
+          { name: props.pos?.display_name }
+        )}>
+        {retrieveDataStatus === null ? (
+          <div className="modal-content">
+            {props.pos?.auth_type !== 'oauth' ? (
+              <>
+                <LabeledInput
+                  type="text"
+                  value={userName}
+                  placeholder="Username"
+                  onChange={(e) => setUserName(e.target.value)}
+                  lighter
+                />
+                <LabeledInput
+                  type="password"
+                  value={userPassword}
+                  placeholder="Password"
+                  onChange={(e) => setUserPassword(e.target.value)}
+                  lighter
+                />
+              </>
+            ) : (
+              <p>
+                {t('onboarding:onboarding.modal.description.oauth.message', {
+                  name: props.pos.display_name,
+                })}
+              </p>
+            )}
+            {errorField && <span className="text-error">{errorField}</span>}
+            <div className="button-container">
+              <Button
+                value={t('cancel')}
+                type="secondary"
+                onClick={props.toggleModal}
               />
-              <LabeledInput
-                type="password"
-                value={userPassword}
-                placeholder="Password"
-                onChange={(e) => setUserPassword(e.target.value)}
-                lighter
+              <Button
+                value={'' + props.pos?.button_display}
+                type="primary"
+                onClick={handleLoginClick}
               />
-            </>
-          ) : (
-            <p>
-              {t('onboarding:onboarding.modal.description.oauth.message', {
-                name: props.pos.display_name,
-              })}
-            </p>
-          )}
-          {errorField && <span className="text-error">{errorField}</span>}
-          <div className="button-container">
-            <Button
-              value={t('cancel')}
-              type="secondary"
-              onClick={props.toggleModal}
-            />
-            <Button
-              value={'' + props.pos?.button_display}
-              type="primary"
-              onClick={handleLoginClick}
-            />
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className="retrieve-data-loading">
-          {retrieveDataStatus === 'loading' ? (
-            <>
-              <Lottie type="loading" width="150px" />
-              <span className="loading-info" id="bold">
-                {t('onboarding:onboarding.recoverData')}
-              </span>
-              <span className="loading-info">
-                {t('onboarding:onboarding.wait')}
-              </span>
-            </>
-          ) : retrieveDataStatus === 'success' ? (
-            <>
-              <Lottie type="validate" width="100px" />
-              <span className="loading-info" id="bold">
-                {t('onboarding:onboarding.success')}
-              </span>
-              <Button
-                value={t('ok')}
-                type="secondary"
-                onClick={() => {
-                  props.toggleModal();
-                  props.onIntegrated(integrated);
-                  setRetrieveDataStatus(null);
-                  setUserName('');
-                  setUserPassword('');
-                }}
-              />
-            </>
-          ) : retrieveDataStatus === 'fail' ? (
-            <>
-              <Lottie type="error" width="100px" />
-              <span className="loading-info" id="bold">
-                {t('onboarding:onboarding.error')}
-              </span>
-              <Button
-                value={t('ok')}
-                type="secondary"
-                onClick={() => {
-                  props.toggleModal();
-                  props.onIntegrated();
-                  setRetrieveDataStatus(null);
-                  setUserName('');
-                  setUserPassword('');
-                }}
-              />
-            </>
-          ) : undefined}
-        </div>
-      )}
-    </Popup>
+        ) : (
+          <div className="retrieve-data-loading">
+            {retrieveDataStatus === 'loading' ? (
+              <>
+                <Lottie type="loading" width="150px" />
+                <span className="loading-info" id="bold">
+                  {t('onboarding:onboarding.recoverData')}
+                </span>
+                <span className="loading-info">
+                  {t('onboarding:onboarding.wait')}
+                </span>
+              </>
+            ) : retrieveDataStatus === 'success' ? (
+              <>
+                <Lottie type="validate" width="100px" />
+                <span className="loading-info" id="bold">
+                  {t('onboarding:onboarding.success')}
+                </span>
+                <Button
+                  value={t('ok')}
+                  type="secondary"
+                  onClick={() => {
+                    props.toggleModal();
+                    props.onIntegrated(integrated);
+                    setRetrieveDataStatus(null);
+                    setUserName('');
+                    setUserPassword('');
+                  }}
+                />
+              </>
+            ) : retrieveDataStatus === 'fail' ? (
+              <>
+                <Lottie type="error" width="100px" />
+                <span className="loading-info" id="bold">
+                  {t('onboarding:onboarding.error')}
+                </span>
+                <Button
+                  value={t('ok')}
+                  type="secondary"
+                  onClick={() => {
+                    props.toggleModal();
+                    props.onIntegrated();
+                    setRetrieveDataStatus(null);
+                    setUserName('');
+                    setUserPassword('');
+                  }}
+                />
+              </>
+            ) : undefined}
+          </div>
+        )}
+      </Popup>
+      <RedCatModal
+        isVisible={redCatModalVisible}
+        setRedCatModalVisible={setRedCatModalVisible}
+        LoginModal={props.toggleModal}
+        toggleModal={redCatModal}
+        setRetrieveDataStatus={setRetrieveDataStatus}
+        redCatData={redCatData}
+      />
+    </>
   );
 };
 
