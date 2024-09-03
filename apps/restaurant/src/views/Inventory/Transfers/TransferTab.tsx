7559030b73602c709274e5cb4b8ea7f06174cc12
Sébastien Pradier <mrsebastienpradier@gmail.com>
2024-09-03 19:02:12 +0200

    update design transfer

--- apps/restaurant/src/views/Inventory/Transfers/TransferTab.tsx
+++ apps/restaurant/src/views/Inventory/Transfers/TransferTab.tsx
@@ -1,39 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TransferHistorySection from './components/TransferHistorySection';
+import { transferService } from '../../../services/transfer.service';
+import { useRestaurantStore } from '../../../store/useRestaurantStore';
 
-export type TransferTabRef = {
-  renderOptions: () => React.ReactNode;
+type Ingredient = {
+  name: string;
+  quantity: number;
+  unit: string;
+  amount: number;
 };
 
-const transferEvents = [
-  {
-    fromSite: 'Site A',
-    toSite: 'Site B',
-    transferDate: '23 Jun 2024',
-    transferredBy: 'John Doe',
-    ingredients: [
-      { name: 'Tomato', quantity: 20, unit: 'kg' },
-      { name: 'Cheese', quantity: 5, unit: 'kg' },
-    ],
-  },
-  {
-    fromSite: 'Site B',
-    toSite: 'Site D',
-    transferDate: '25 Jun 2024',
-    transferredBy: 'Jane Smith',
-    ingredients: [
-      { name: 'Lettuce', quantity: 10, unit: 'kg' },
-      { name: 'Bread', quantity: 30, unit: 'pcs' },
-    ],
-  },
-];
+type TransferEvent = {
+  fromSite: string;
+  toSite: string;
+  transferDate: string;
+  transferredBy: string;
+  transferAmount: number;
+  ingredients: Ingredient[];
+};
 
 export const TransferTab = () => {
+  const [transferEvents, setTransferEvents] = useState<TransferEvent[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const selectedRestaurantUUID = useRestaurantStore(
+    (state) => state.selectedRestaurantUUID
+  );
+
+  useEffect(() => {
+    if (!selectedRestaurantUUID) return;
+  });
+
+  useEffect(() => {
+    const fetchTransferHistory = async () => {
+      try {
+        const data = await transferService.getTransferHistory(
+          selectedRestaurantUUID
+        ); // replace with actual restaurantId
+        const formattedData = data.map((item: any) => item.transfer_event); // Extract the transfer_event object
+        setTransferEvents(formattedData);
+      } catch (err) {
+        console.error('Failed to fetch transfer history:', err);
+        setError('Failed to fetch transfer history');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTransferHistory();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
     <div>
-      {transferEvents.map((event, index) => (
-        <TransferHistorySection key={index} event={event} />
-      ))}
+      {transferEvents.length > 0 ? (
+        transferEvents.map((event, index) => (
+          <TransferHistorySection key={index} event={event} />
+        ))
+      ) : (
+        <div>No transfer history available.</div>
+      )}
     </div>
   );
 };
--- apps/restaurant/src/views/Inventory/Transfers/components/TransferHistorySection.tsx
+++ apps/restaurant/src/views/Inventory/Transfers/components/TransferHistorySection.tsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
 import styles from './TransferHistorySection.module.scss';
+import { useRestaurantStore } from '../../../../store/useRestaurantStore';
 
 type Ingredient = {
   name: string;
   quantity: number;
   unit: string;
+  amount: number;
 };
 
 type TransferEvent = {
   fromSite: string;
   toSite: string;
+  fromSiteUUID: string;
+  toSiteUUID: string;
   transferDate: string;
   transferredBy: string;
+  transferAmount: number;
   ingredients: Ingredient[];
 };
 
@@ -28,14 +33,30 @@ const TransferHistorySection: React.FC<TransferHistorySectionProps> = ({
     setIsIngredientsVisible(!isIngredientsVisible);
   };
 
+  const selectedRestaurantUUID = useRestaurantStore(
+    (state) => state.selectedRestaurantUUID
+  );
+  console.log('check selectedRestaurantUUID');
+  console.log(selectedRestaurantUUID);
+  console.log('check event');
+  console.log(event.fromSiteUUID);
+  console.log(event.toSiteUUID);
+
+  const titleClass =
+    event.fromSiteUUID === selectedRestaurantUUID
+      ? styles.sending
+      : event.toSiteUUID === selectedRestaurantUUID
+      ? styles.receiving
+      : '';
+
   return (
     <div className={styles.transferHistorySection}>
       <div className={styles.header}>
-        <h3>
-          {event.fromSite === 'Site B'
-            ? `To ${event.toSite}`
-            : event.toSite === 'Site B'
-            ? `From ${event.fromSite}`
+        <h3 className={titleClass}>
+          {event.fromSiteUUID === selectedRestaurantUUID
+            ? `Sent to ${event.toSite}`
+            : event.toSiteUUID === selectedRestaurantUUID
+            ? `Received from ${event.fromSite}`
             : `Transfer from ${event.fromSite} to ${event.toSite}`}
         </h3>
         <p className={styles.date}>{event.transferDate}</p>
@@ -44,7 +65,10 @@ const TransferHistorySection: React.FC<TransferHistorySectionProps> = ({
         <p>
           {event.transferredBy
             ? `Transferred by ${event.transferredBy}`
-            : 'Transferred by unknown'}
+            : 'Transferred by -'}
+        </p>
+        <p>
+          Total Amount: <b>€{event.transferAmount.toFixed(2)}</b>
         </p>
         <div
           className={styles.ingredientsToggle}
@@ -65,6 +89,7 @@ const TransferHistorySection: React.FC<TransferHistorySectionProps> = ({
                     ? `${ingredient.quantity} ${ingredient.unit}s` // pluralize unit if quantity > 1
                     : `${ingredient.quantity} ${ingredient.unit}`}
                 </span>
+                <span> - €{ingredient.amount.toFixed(2)}</span>
               </li>
             ))}
           </ul>
