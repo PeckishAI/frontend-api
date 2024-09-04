6ac6451456257e28c6529a637d5b57f9fa1f61e0
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 <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>
2024-09-04 14:22:50 +0530

    feat(updated)- Updated the supplier UI

--- apps/restaurant/src/views/Inventory/Suppliers/components/AddSupplierPopup.tsx
+++ apps/restaurant/src/views/Inventory/Suppliers/components/AddSupplierPopup.tsx
@@ -21,7 +21,7 @@ const AddSupplierSchema = z.object({
     .string()
     .optional()
     .or(z.null().transform(() => undefined)),
-  automaticInvitation: z.boolean(),
+  // automaticInvitation: z.boolean(),
 });
 
 type SupplierForm = z.infer<typeof AddSupplierSchema>;
@@ -52,9 +52,9 @@ const AddSupplierPopup = (props: Props) => {
     reset,
   } = useForm<SupplierForm>({
     resolver: zodResolver(AddSupplierSchema),
-    defaultValues: {
-      automaticInvitation: true,
-    },
+    // defaultValues: {
+    //   automaticInvitation: true,
+    // },
   });
 
   useEffect(() => {
@@ -119,11 +119,11 @@ const AddSupplierPopup = (props: Props) => {
           </>
         </div>
 
-        <Checkbox
+        {/* <Checkbox
           label={t('suppliers.addSupplier.automaticSendCheckbox')}
           className={styles.autoCheckbox}
           {...register('automaticInvitation')}
-        />
+        /> */}
 
         <div className={styles.buttonsContainer}>
           <Button
