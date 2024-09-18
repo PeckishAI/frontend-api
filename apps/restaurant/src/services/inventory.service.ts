57ef6158cae0fa6f0ac01e0d4cc968aacd0803e3
Sébastien Pradier <mrsebastienpradier@gmail.com>
2024-09-19 00:30:03 +0200

    check logs

--- apps/restaurant/src/services/inventory.service.ts
+++ apps/restaurant/src/services/inventory.service.ts
@@ -108,25 +108,31 @@ const getIngredientList = async (
   console.log('INGREDIENTS');
   console.log(res.data);
 
-  const ingredients = Object.keys(res.data).map<Ingredient>((key) => ({
-    id: key,
-    name: res.data[key]['name'],
-    parLevel: res.data[key]['par_level'],
-    actualStock: res.data[key]['actual_stock'],
-    theoriticalStock: res.data[key]['theoritical_stock'],
-    unit: res.data[key]['unit'],
-    unitCost: res.data[key]['cost'],
-    tagUUID: res.data[key]['tag_uuid']?.map((uuid: string) => uuid) || [],
-    supplier_details: res.data[key]['supplier_details'].map(
-      (supplier: any) => ({
-        supplier_id: supplier['supplier_id'],
-        supplier_name: supplier['supplier_name'],
-        supplier_cost: supplier['supplier_cost'],
-      })
-    ),
-    amount: res.data[key]['amount'],
-    type: res.data[key]['type'],
-  }));
+  const ingredients = Object.keys(res.data).map<Ingredient>((key) => {
+    try {
+      const data = res.data[key];
+      return {
+        id: key,
+        name: data['name'],
+        parLevel: data['par_level'],
+        actualStock: data['actual_stock'],
+        theoriticalStock: data['theoritical_stock'],
+        unit: data['unit'],
+        unitCost: data['cost'],
+        tagUUID: data['tag_uuid']?.map((uuid: string) => uuid) || [],
+        supplier_details: data['supplier_details'].map((supplier: any) => ({
+          supplier_id: supplier['supplier_id'],
+          supplier_name: supplier['supplier_name'],
+          supplier_cost: supplier['supplier_cost'],
+        })),
+        amount: data['amount'],
+        type: data['type'],
+      };
+    } catch (error) {
+      console.error(`Error processing key: ${key}`, error);
+      return null; // Optionally return null or some default value
+    }
+  });
 
   console.log(ingredients);
 
