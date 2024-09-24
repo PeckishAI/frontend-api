ed70239ee81215d798ce3e925ec3fdcedab339ca
mayurk <mailto:mayur.karmur@iampeckish.com>
2024-09-24 09:57:25 +0530

    fix(Quick-fix)- Multiple tag design issued

--- apps/restaurant/src/views/Inventory/Ingredients/AddIngredientPopup.tsx
+++ apps/restaurant/src/views/Inventory/Ingredients/AddIngredientPopup.tsx
@@ -383,13 +383,33 @@ const AddIngredientPopup = (props: Props) => {
                     : provided.backgroundColor,
                   color: state.isSelected ? '#FFFFFF' : provided.color,
                 }),
+                multiValue: (provided) => ({
+                  ...provided,
+                  backgroundColor: '#5E72E4',
+                  color: '#FFFFFF',
+                  borderRadius: '12px',
+                }),
+                multiValueLabel: (provided) => ({
+                  ...provided,
+                  color: '#ffffff',
+                  borderRadius: '12px',
+                }),
+                multiValueRemove: (provided) => ({
+                  ...provided,
+                  color: '#ffffff',
+                  ':hover': {
+                    backgroundColor: '#b5adad',
+                    borderRadius: '12px',
+                    color: '#ffffff',
+                  },
+                }),
               }}
               isClearable
             />
           </div>
 
           {/* Supplier Details Section */}
-          <h3>Supplier Details</h3>
+          <h3>Supplier Details </h3>
 
           {supplierFields.map((field, index) => (
             <div key={field.id} className={styles.SupplierSection}>
