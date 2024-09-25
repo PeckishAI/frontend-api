202ae255306e166c238672205db5baa6f73ed620
Sébastien Pradier <mrsebastienpradier@gmail.com>
2024-09-25 14:03:17 +0200

    check build

--- apps/restaurant/src/views/MyRestaurant.tsx
+++ apps/restaurant/src/views/MyRestaurant.tsx
@@ -7,11 +7,10 @@ import { useUserStore } from '@peckishai/user-management';
 import { toast } from 'react-hot-toast';
 
 const MyRestaurant = () => {
-  const navigate = useNavigate();
-
   const { t } = useTranslation();
   useTitle(t('pages.myRestaurants'));
 
+  const navigate = useNavigate();
   const { restaurants, loadRestaurants, setSelectedRestaurantUUID } =
     useRestaurantStore();
   const { user } = useUserStore(); // Get the user from the user store
