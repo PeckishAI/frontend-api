0fab62e91eed0ee38d2371da7565c3cd719992ff
Sébastien Pradier <57252280+spradier@users.noreply.github.com>
2024-12-18 22:15:19 +0000

    Restored to 'eafc52ce611bc232705da770df3489cdfbb65ee8'
    Replit-Restored-To:eafc52ce611bc232705da770df3489cdfbb65ee8

--- client/src/components/inventory/EditIngredientForm.tsx
+++ client/src/components/inventory/EditIngredientForm.tsx
@@ -424,7 +424,7 @@ export default function EditIngredientForm({
                   .watch("ingredient_suppliers")
                   ?.map((ingredientSupplier, index) => (
                     <div
-                      key={ingredientSupplier.uuid}
+                      key={ingredientSupplier.supplier.supplier_uuid}
                       className="border border-gray-200 bg-white p-4 rounded-lg shadow-sm"
                     >
                       {editingSupplier === index ? (
@@ -530,7 +530,7 @@ export default function EditIngredientForm({
                                   <FormControl>
                                     <CreatableSelect
                                       value={
-                                        field.value?.unit_name
+                                        field.value?.unit_name 
                                           ? [field.value.unit_name]
                                           : []
                                       }
@@ -615,13 +615,7 @@ export default function EditIngredientForm({
                               type="button"
                               variant="outline"
                               size="sm"
-                              onClick={() => {
-                                setEditingSupplier(null);
-                                const values = form.getValues();
-                                if (form.formState.isValid) {
-                                  handleSubmit(values);
-                                }
-                              }}
+                              onClick={() => setEditingSupplier(null)}
                             >
                               Done
                             </Button>
@@ -668,7 +662,7 @@ export default function EditIngredientForm({
                               </div>
                               <div className="font-medium">
                                 {ingredientSupplier.pack_size}
-                              </div>
+                              </div> 
                             </div>
                           </div>
                         </div>
